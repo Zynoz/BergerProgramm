e870546439c17ac6203a217e931cfe859e844fa7
Maxi <maximilian.moser-cic@ibm.com>
2019-05-18 14:43:00 +0200

    added Util test structure

---
+++ src/test/java/model/utils/MatrixUtilsTest.java
@@ -0,0 +1,18 @@
+package model.utils;
+
+import static org.junit.Assert.*;
+
+public class MatrixUtilsTest {
+
+    @org.junit.Test
+    public void isValidMatrix() {
+    }
+
+    @org.junit.Test
+    public void isValidSize() {
+    }
+
+    @org.junit.Test
+    public void convertToMatrix() {
+    }
+}
